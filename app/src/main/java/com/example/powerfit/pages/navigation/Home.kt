02cb4f958567cb75f3da68ca9b328031bb0064df
valeriaAnabeldePrado <anabeltayu@gmail.com>
2024-06-24 00:53:26 -0300

    Agregue modal log out

--- app/src/main/java/com/example/powerfit/pages/navigation/Home.kt
+++ app/src/main/java/com/example/powerfit/pages/navigation/Home.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.widget.AppCompatButton
 import com.example.powerfit.R
 
 // TODO: Rename parameter arguments, choose names that match
