4e3d1d009644451164640b5f0bf4fa2a0279541e
valeriaAnabeldePrado <anabeltayu@gmail.com>
2024-06-28 12:42:09 -0300

    creando el dropdown pago cuota

--- app/src/main/java/com/example/powerfit/pages/components/navModals/PagoCuotaCard.kt
+++ app/src/main/java/com/example/powerfit/pages/components/navModals/PagoCuotaCard.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.powerfit.R
@@ -45,6 +46,7 @@ class PagoCuotaCard : Fragment() {
 
         val adapterPage = FragmentPageAdapter(childFragmentManager, lifecycle, fragmentsGroup)
         viewPageFragments.adapter = adapterPage
+
 
 
         return view
